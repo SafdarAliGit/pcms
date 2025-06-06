020c7e22f11af83cf11b1a467859e0e03b60dacf
SafdarAliGit <safdar211@gmail.com>
2025-06-06 18:40:09 +0500

    updated

--- pcms/api/notify_new_message.py
+++ pcms/api/notify_new_message.py
@@ -3,7 +3,7 @@ import frappe
 def notify_new_message(doc, method):
     frappe.publish_realtime("new_message", {
         "message_content": doc.message_content,
-        "sender": doc.mr_no,
+        "sender": doc.sender,
         "sender_name": doc.sender_name,
         "room_no": doc.room_no,
         "status": doc.status,
