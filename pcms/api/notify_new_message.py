96206df47f4976929eebad6a1e2fd48ff83481a4
SafdarAliGit <safdar211@gmail.com>
2025-06-06 11:08:18 +0500

    udpatged

--- pcms/api/notify_new_message.py
+++ pcms/api/notify_new_message.py
@@ -1,7 +1,6 @@
 # in your_app/api/messaging.py
 import frappe
 def notify_new_message(doc, method):
-    frappe.throw("New Message")
     frappe.publish_realtime("new_message", {
         "message": doc.message_content,
         "sender": doc.owner
