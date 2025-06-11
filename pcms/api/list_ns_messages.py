c9383c6684aed429b76c466a73e7f1c7c53da502
SafdarAliGit <safdar211@gmail.com>
2025-06-11 21:04:00 +0500

    updated

--- pcms/api/list_ns_messages.py
+++ pcms/api/list_ns_messages.py
@@ -2,7 +2,7 @@ import frappe
 
 @frappe.whitelist()
 def list_ns_messages():
-    # Get patient using the current logged-in user's ID
+    # Get nursing station based on the current logged-in user's ID
     nursing_station = frappe.db.get_value(
         "Nursing Station", 
         {"user_id": frappe.session.user}, 
@@ -10,18 +10,18 @@ def list_ns_messages():
         as_dict=True
     )
 
-    if not nurse:
-        frappe.throw("No patient found for this user.")
+    if not nursing_station:
+        frappe.throw("No nursing station found for this user.")
 
-    # Fetch messages sent by this patient (using mr_no as sender)
+    # Fetch messages linked to this nursing station
     messages = frappe.db.get_all(
         "Message",
         filters={"nursing_station": nursing_station.name},
-        fields=["message_content", "sender","sender_name","room_no","sent_time","status","audio"],order_by="creation asc"
+        fields=[
+            "message_content", "sender", "sender_name", 
+            "room_no", "sent_time", "status", "audio"
+        ],
+        order_by="creation asc"
     )
 
-    return messages  # Return list (can be empty)
-
-       
-
-    
+    return messages  # Always returns a list, can be empty
