18b4add44e15c83b71eac4e4549ffc93d7267e65
SafdarAliGit <safdar211@gmail.com>
2025-06-12 14:14:33 +0500

    updated

--- pcms/api/get_nursing_station.py
+++ pcms/api/get_nursing_station.py
@@ -4,7 +4,7 @@ import frappe
 def get_nursing_station(**args):
         station = frappe.db.get_value(
             "Nursing Station", 
-            {"user_id": args["user"]}, 
+            {"user_id": args.get("user")}, 
             "name"
         )
         return station    
