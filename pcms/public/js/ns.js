307a4fef198f323d19ebdb2b3c8cc0a6563628c0
SafdarAliGit <safdar211@gmail.com>
2025-06-12 14:18:48 +0500

    updated

--- pcms/public/js/ns.js
+++ pcms/public/js/ns.js
@@ -50,7 +50,7 @@ frappe.ready(function () {
 // 2. Independent realtime setup
   const room = nursing_station.name.replace(/[-\s]/g, "").toLowerCase();
 
-
+console.log(room);
   frappe.realtime.on(room, function (data) {
     appendMessage(data.message_content, data.sender,data.sender_name,data.room_no,data.sent_time,data.status,data.audio);
     // playNotificationSound();
