587045f5de089c2b395900086bb945a0bc9a7f07
SafdarAliGit <safdar211@gmail.com>
2025-06-12 14:11:55 +0500

    updated

--- pcms/api/get_nursing_station.py
+++ pcms/api/get_nursing_station.py
@@ -1,10 +1,10 @@
 import frappe
 
 @frappe.whitelist()
-def get_nursing_station(user):
+def get_nursing_station(**args):
         station = frappe.db.get_value(
             "Nursing Station", 
-            {"user_id": user}, 
+            {"user_id": args["user"]}, 
             "name"
         )
         return station    
