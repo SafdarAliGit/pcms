535d005c1aa69c452b46bbf6a5bacc7e5aef7cec
SafdarAliGit <safdar211@gmail.com>
2025-06-13 20:54:41 +0500

    updated

--- pcms/www/chat/index.py
+++ pcms/www/chat/index.py
@@ -3,18 +3,20 @@ import frappe
 def get_context(context):
     context.no_cache = 1
     user = frappe.session.user
-    context.patient = {}
 
-    if user != "Guest":
-        patient = frappe.db.get_value(
+    if user and user != "Guest":
+        patient = frappe.db.get_list(
             "Patient",
             {"user_id": user},
-            ["patient_name", "mr_no"],
+            ["patient_name", "mr_no", "nursing_station"],
             as_dict=True,
-            order_by="creation desc"
+            order_by="creation desc",
+            limit=1
         )
         if patient:
-            context.patient = patient
+            context["patient"] = patient[0].patient_name
+            context["mr_no"] = patient[0].mr_no
+            context["nursing_station"] = patient[0].nursing_station
 
     return context
 
