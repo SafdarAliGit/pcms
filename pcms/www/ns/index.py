2a848e74679cf220cab0f0c804dc628f60931d75
SafdarAliGit <safdar211@gmail.com>
2025-06-15 13:56:56 +0500

    updated

--- pcms/www/ns/index.py
+++ pcms/www/ns/index.py
@@ -1,17 +1,17 @@
-import frappe
-from frappe import _
+# import frappe
+# from frappe import _
 
-def get_context(context):
-    context.no_cache = 1
-    user = frappe.session.user or ""
-    context.station = ""
+# def get_context(context):
+#     context.no_cache = 1
+#     user = frappe.session.user or ""
+#     context.station = ""
 
-    if user != "Guest" and user:
-        try:
-            station = frappe.db.get_value("Nursing Station", {"user_id": user}, "name")
-            context.station = station or ""
-        except Exception as e:
-            frappe.log_error(frappe.get_traceback(), _("Error fetching Nursing Station for user {0}").format(user))
-            context.station = ""
+#     if user != "Guest" and user:
+#         try:
+#             station = frappe.db.get_value("Nursing Station", {"user_id": user}, "name")
+#             context.station = station or ""
+#         except Exception as e:
+#             frappe.log_error(frappe.get_traceback(), _("Error fetching Nursing Station for user {0}").format(user))
+#             context.station = ""
 
-    return context
+#     return context
